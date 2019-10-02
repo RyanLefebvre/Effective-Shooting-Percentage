c2ee6a4e191276a544bb05ad599e15c305ae194e
Ryan <ral1036@wildcats.unh.edu>
2019-10-02 12:58:15 -0400

    implemented makeTeam function

--- backend/effectiveShootingPercentage.py
+++ backend/effectiveShootingPercentage.py
@@ -29,6 +29,16 @@ class Player(object):
                 "\t Shots: " + str(self.totalShotAttempts) + "\tES%: " + 
                 str( self.effectiveShootingPercentage) )
         
+#class for holding information about a team 
+class Team(object):
+    name = ""
+    league = ""
+    onePointGoals = 0
+    twoPointGoals = 0
+    totalShotAttempts = 0
+    effectiveShootingPercentage = 0.0 
+    roster = []
+        
 
 #function for calculating ES% of a player
 def getEffectiveShootingPercentage( onePointGoals ,
@@ -59,17 +69,42 @@ def makePlayer( firstName , lastName , team ,  onePointGoals,
     return newPlayer 
 
 
-def getPLLPlayerData( playerList):
+def makeTeam( teamName , league ):
+    newTeam = Team()
+    newTeam.name = teamName
+    newTeam.league = league
+    return newTeam
+
+def addPlayerToTeam( player , teamName  , teams ):
+    # first check if the team does not already exist 
+    if( teams[teamName] == None  ):
+        # if not then make a new team
+        teams[teamName] =  makeTeam( teamName , player.league ) 
+    #add player to teams roster and update stats
+    teams[teamName].roster.append( player )
+    teams[teamName].onePointGoals += player.onePointGoals
+    teams[teamName].twoPointGoals += player.twoPointGoals 
+    teams[teamName].totalShotAttempts += player.totalShotAttempts
+    
+    ##STILL NEED TO CALCULATE ES% FOR THIS TO WORK
+    
+    # now add player to the teams roster and update the teams stats 
+    listOfTeams
+
+def getPLLPlayerData( playerList , teamDict ):
     pll_response = requests.get("https://dn0a11v09sa0t.cloudfront.net/" + 
                                 "SeasonTeamAndPlayersStats.json")
     pllSoup = BeautifulSoup( pll_response.text ,"lxml" ).find("p").text
     pllJson = json.loads( pllSoup )
     for team in pllJson['Teams']:
        for player in team['Players']:
-           playerList.append(
-                   makePlayer( player['FirstName'] , player['LastName'] , player['TeamId'] , 
-                      player['OnePointGoals'] , player['TwoPointGoals'] , eval(player['Shots']),
-                            "PLL" , player["Position"] ) )    
+           newPlayer = ( makePlayer( 
+                   player['FirstName'] , player['LastName'],
+                   player['TeamId'], player['OnePointGoals'], 
+                   player['TwoPointGoals'] , eval(player['Shots']),
+                   "PLL" , player["Position"] ))
+           playerList.append( newPlayer )
+           addPlayerToTeam( newPlayer, newPlayer.team , teamDict )
     return
 
 def getMLLData( playerList ):
@@ -83,8 +118,8 @@ try:
     
     #contains a list of all players currently playing professional lacrosse 
     playerList = []
-    teamList = []
-    getPLLPlayerData( playerList )      
+    teamDict  = {}
+    getPLLPlayerData( playerList , teamDict )      
     
 ##################  need to get stats for MLL, then iterate through all players and construct #####################################
 ################### stats for teams , based on this see if we can predict who wins games  ######################################### 
