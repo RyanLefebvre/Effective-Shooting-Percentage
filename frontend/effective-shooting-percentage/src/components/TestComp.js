00ec830bd1156f22c454aa26a39f99001c696e9a
Ryan <ral1036@wildcats.unh.edu>
2019-10-12 15:36:10 -0400

    Created basic react component, removed boilerplate structure

---
+++ frontend/effective-shooting-percentage/src/components/TestComp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function TestComp() {
+  return (
+    <div className="TestComp">
+      <h1> TestComp </h1>
+    </div>
+  );
+}
+
+
+export default TestComp;
