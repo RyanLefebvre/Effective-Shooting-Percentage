114fc17439e521ea2ece0d410f4c94658f4431de
Ryan <ral1036@wildcats.unh.edu>
2019-10-16 21:55:09 -0400

    Updates

---
+++ precache-manifest.44e16ceaf1e1a83ec9208fd2e227b693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216511c7417de4aadac2abc222d30cbc",
+    "url": "/effectiveShootingPercentage/index.html"
+  },
+  {
+    "revision": "4a406db6b8081db4e1b6",
+    "url": "/effectiveShootingPercentage/static/css/main.09d313b6.chunk.css"
+  },
+  {
+    "revision": "0d08b48024294c8c843b",
+    "url": "/effectiveShootingPercentage/static/js/2.2220b9bf.chunk.js"
+  },
+  {
+    "revision": "4a406db6b8081db4e1b6",
+    "url": "/effectiveShootingPercentage/static/js/main.0be03365.chunk.js"
+  },
+  {
+    "revision": "73bf99fcf99d73dc9caf",
+    "url": "/effectiveShootingPercentage/static/js/runtime-main.957c16e3.js"
+  },
+  {
+    "revision": "90099611d8f0fb95d291789a2baedea1",
+    "url": "/effectiveShootingPercentage/static/media/2pg.90099611.jpg"
+  },
+  {
+    "revision": "d67307159371eaf248cff516c264defb",
+    "url": "/effectiveShootingPercentage/static/media/formula.d6730715.PNG"
+  }
+]);
