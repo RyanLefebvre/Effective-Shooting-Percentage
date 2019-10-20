a200df6ce2215a11c644b97290c1f47220c0fe33
Ryan <ral1036@wildcats.unh.edu>
2019-10-20 13:06:57 -0400

    wrote player section of data page

--- frontend/effective-shooting-percentage/src/components/data.js
+++ frontend/effective-shooting-percentage/src/components/data.js
@@ -5,8 +5,13 @@ import beautifulSoup from '../images/beautifulSoup.png'
 import trevor from '../images/trevor-baptiste.jpg'
 import teams from '../images/teams.jpg'
 import games from '../images/games.jpg'
+import playerData from '../sourceCode/players.csv'
 
 function DataComp() {
+
+  const linkStyle = {
+    textDecoration: 'underline'
+  }
 
   const imgWrapper = {
     display:'block',
@@ -27,7 +32,7 @@ function DataComp() {
     display:'flex',
     jusitfyContent:'center',
     flexWrap: 'wrap',
-    maxWidth:'1100px',
+    maxWidth:'1200px',
     margin:'auto'
   }
 
@@ -35,7 +40,8 @@ function DataComp() {
     display:'block',
     margin:'auto',
     maxWidth: '700px',
-    minWidth:'60%'
+    minWidth: '320px',
+    width:'65%'
   }
 
   return(    
@@ -43,29 +49,48 @@ function DataComp() {
     <h1 className="headerStyles"> Data Collection </h1>
 
     <div style={flexManagerStyles} class ="flexManager">
-    <Card style={flexChildBigStyles} className="cardContentStyles">
+      <div style={flexChildBigStyles}>
+      <Card  className="cardContentStyles">
         <CardContent>
           Unlike other professional sports, lacrosse has a relatively small following.
           The data used for this project was not readily available because of this. 
           There are currently two professional field lacrosse leagues, the PLL and MLL.
-          Each league maintains player and team statistics on their websites. I used the 
-          'Beautiful Soup' Python library to extract and aggregate data for both leagues.
+          Each league maintains player and team statistics on their websites. 
+          <br></br><br></br>
+          Unfortunately there is no publicly available data for previous seasons statistics, 
+          all data is from the 2019 PLL and MLL seasons.I used the 'Beautiful Soup' Python library
+           and general knowledge of website architecture to extract and aggregate data for both leagues.
         </CardContent>
       </Card>
+      </div>
+  
       <div style ={imgWrapper}>
         <img  style={imgStyles} src ={beautifulSoup} alt="beautifulSoup"></img>
       </div>
     </div>
 
-    <br></br>
+    <br></br> <br></br>
     
       <div style={flexManagerStyles} class ="flexManager">
         <div style={flexChildBigStyles}>
           <h1 className="headerStyles"> Player Data </h1>
             <Card  className="cardContentStyles">
               <CardContent>
-                This is a long sentence that will cause a line break. if there is 
-                insufficient content in the card then it is not styled correctly
+                Between the PLL and MLL there were 388 professional lacrosse players in 2019. 
+                The dataset I compiled as a result of scraping both leagues sites for info only contains
+                statistics relevant to calculating ES%. More statistics are available with the raw data.
+                <br></br><br></br>
+                PLL player data was easy to compile by parsing through the JSON returned from making a GET 
+                request to the following URL:  
+                <a href ="https://dn0a11v09sa0t.cloudfront.net/SeasonTeamAndPlayersStats.json" style={linkStyle}>PLL JSON</a>
+                <br></br><br></br>
+                Data for the MLL was more difficult to gather. Their website was not set up to easily scrape player statistics.
+                However, by watching the network tab in google chrome's developer tools I was able to identify they were loading a component on 
+                their statistics page from another site (<a style={linkStyle} href="https://iframe.faststats.online/">FastStats</a>). FastStats, appears to be the MLL's database 
+                for player and team statistics and I was able to scrape player statistics from this site.
+
+                <br></br><br></br>
+                2019 PLL/MLL player Dataset: <a href ={playerData} style={linkStyle}>players.csv</a>
               </CardContent>
             </Card>
         </div>
@@ -74,7 +99,7 @@ function DataComp() {
         </div>    
       </div>
 
-    <br></br>
+      <br></br> <br></br>
     
     <div style={flexManagerStyles} class ="flexManager">
       <div style={flexChildBigStyles}>
@@ -91,7 +116,7 @@ function DataComp() {
       </div>    
    </div>
 
-   <br></br>
+   <br></br> <br></br>
     
     <div style={flexManagerStyles} class ="flexManager">
       <div style={flexChildBigStyles}>
@@ -107,11 +132,6 @@ function DataComp() {
         <img  style={imgStyles} src ={games} alt ="trevor-baptiste"></img>
       </div>    
    </div>
-
-
-
-
-
     </div>    
     );  
 }
